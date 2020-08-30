cae5a024cb1696c19379e9c8920ea4e39de1c01d
ajmalfaiz <ajmalfaiz5@gmail.com>
2020-08-31 01:16:39 +0530

    REST API almost

---
+++ backend/products/views.py
@@ -0,0 +1,43 @@
+from django.shortcuts import render
+from rest_framework.response import Response
+from rest_framework.views import APIView
+from rest_framework import status, exceptions
+from rest_framework.viewsets import ModelViewSet
+from rest_framework.generics import ListAPIView 
+from .models import Product, Seller, Category
+from .serializers import  ProductsAllInfoSerializer,CategorySerializer,ProductSerializer, SellerSerializer
+
+
+class ProductList(APIView):
+    
+   
+    # List and Create all products
+    def get(self, request):
+        products = Product.objects.all()
+        serializer = ProductsAllInfoSerializer(products, many=True)
+        return Response(serializer.data)
+    
+    def post(self, request):
+        serializer = ProductSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+class ProductListAPIView(ListAPIView):
+    serializer_class = ProductSerializer
+    queryset = Product.objects.all()
+    filter_fields = (
+        'category_id',
+    )
+    search_fields = (
+        'title',
+    )
+
+class CategoryViewSet(ModelViewSet):
+    serializer_class = CategorySerializer
+    queryset = Category.objects.all()
+
+class SellerViewSet(ModelViewSet):
+    serializer_class = SellerSerializer
+    queryset = Seller.objects.all()
